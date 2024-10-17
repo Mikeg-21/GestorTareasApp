1ffcafb172693cabf4337e35c1898d430f22a225
GUTY <mikegutierrez521@gmail.com>
2024-10-16 22:54:20 -0500

    primer commit app funcionando al pelo

---
+++ com.gestor.tareas/src/main/java/com/gestor/tareas/com/gestor/tareas/security/SecurityConfiguration.java
@@ -0,0 +1,68 @@
+package com.gestor.tareas.com.gestor.tareas.security;
+
+import com.gestor.tareas.com.gestor.tareas.service.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@EnableWebSecurity
+@Component
+public class SecurityConfiguration {
+
+    private final IUsuarioService usuarioServicio;
+
+    @Autowired
+    public SecurityConfiguration(IUsuarioService usuarioServicio) {
+        this.usuarioServicio = usuarioServicio;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(usuarioServicio);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/registro","/login", "/js/**", "/css/**", "/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.loginPage("/login")
+                        .defaultSuccessUrl("/tareas", true)
+                        .permitAll()
+                )
+                .logout((logout) -> logout
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
