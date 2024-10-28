9c63d42791b9db9672ffdb0a7e6537a8625f8c5a
GUTY <mikegutierrez521@gmail.com>
2024-10-27 23:30:28 -0500

    Subiendo recordatorios a email, aun no llegan a email, hay una clase de bug

--- com.gestor.tareas/src/main/java/com/gestor/tareas/com/gestor/tareas/AppGestorTareasApplication.java
+++ com.gestor.tareas/src/main/java/com/gestor/tareas/com/gestor/tareas/AppGestorTareasApplication.java
@@ -2,7 +2,11 @@ package com.gestor.tareas.com.gestor.tareas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
+@EnableScheduling
 @SpringBootApplication
 public class AppGestorTareasApplication {
 
